46dd6b19913b1602eab9c39510428974f1a958eb
Andrey373605 <martinovich2005@gmail.com>
2026-05-04 16:38:33 +0300

    chore: migrate infrastructure modules

---
+++ shop/app/application/use_cases/product_details/__init__.py
@@ -0,0 +1,10 @@
+from .commands import CreateProductDetailsUseCase, UpdateProductDetailsUseCase, DeleteProductDetailsUseCase
+from .queries import GetProductDetailsByIdUseCase, ListProductDetailsUseCase
+
+__all__ = [
+    "CreateProductDetailsUseCase",
+    "UpdateProductDetailsUseCase",
+    "DeleteProductDetailsUseCase",
+    "GetProductDetailsByIdUseCase",
+    "ListProductDetailsUseCase",
+]
